b782cec09fd680b8dbe5b8610a62c99257abab87
Jerome Nourse <jdnourse@MacBook-Pro.local>
2021-12-30 16:00:06 -0700

    first commit

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -6,18 +6,18 @@ readFile("./content/first.txt", "utf-8", (err, result) => {
     return;
   }
   const first = result;
-  readFile("./content/second.txt", "utf-8", (err, result) => {
-    if (err) {
-      console.log(err);
+  readFile("./content/second.txt", "utf-8", (err2, result2) => {
+    if (err2) {
+      console.log(err2);
       return;
     }
-    const second = result;
+    const second = result2;
     writeFile(
       "./content/result-async.txt",
       `Here is the result: ${first}, ${second}`,
-      (err, result) => {
-        if (err) {
-          console.log(err);
+      (err3, result3) => {
+        if (err3) {
+          console.log(err3);
           return;
         }
         console.log("done with this task");
