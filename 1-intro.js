50b8410f2bdd4385205fab31d378f96b6bae0f15
jdnourse <jdnourse@me.com>
2022-01-16 13:28:37 -0800

    added some files and edited some

--- 1-intro.js
+++ 1-intro.js
@@ -1,7 +1,7 @@
 const amount = 9;
 
 if (amount < 10) {
-  console.log("small number");
+  console.log("Small number");
 } else {
   console.log("large number");
 }
--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-// npm - global command, comes with node
-// npm --version
+const EventEmitter = require("events");
 
-// local dependency - use it only in this particular project
-//  npm i <packageName>
+const customEmitter = new EventEmitter();
 
-// global dependency - use it in any project
-// npm install -g <packageName>
-// sudo npm install -g <packageName> (mac)
-
-// package.json - manifest file (stores important info about project/package)
-// manual approach (create pacage.json in the root, create properties etc)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
-
-const _ = require("lodash");
-
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
-console.log("Hello world");
+customEmitter.on("response", (name, id) => {
+  console.log(`data received from ${name}, id: ${id}`);
+});
+customEmitter.on("response", (name, id) => {
+  console.log(`some other data received`);
+});
+customEmitter.emit("response", "john", 34);
